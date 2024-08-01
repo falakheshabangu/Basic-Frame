9a687bf40e40480213c9430cfec624edcfb99d42
falakheshabangu <161919888+falakheshabangu@users.noreply.github.com>
2024-08-01 13:37:46 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,12 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        MyFirstFrame myFrame = new MyFirstFrame();
+    }
+    
+}
---
+++ MyFirstFrame.java
@@ -0,0 +1,21 @@
+
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class MyFirstFrame extends JFrame{
+
+    public MyFirstFrame() {
+        setTitle("My First Frame");
+        setSize(200, 250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.blue);
+        setVisible(true);
+    }
+    
+    
+    
+}
